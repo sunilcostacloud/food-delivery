ff42a7949679b227ea01c72dd2212fe011384fcf
sunil <sunil@costacloud.com>
2024-06-12 17:54:39 +0530

    update

--- frontend/src/redux/baseURL/baseURL.ts
+++ frontend/src/redux/baseURL/baseURL.ts
@@ -1,1 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_BASE_DEV_URL
+    : import.meta.env.VITE_API_BASE_PROD_URL;
