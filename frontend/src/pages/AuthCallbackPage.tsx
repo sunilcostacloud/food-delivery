c87dd9d28d8ab9551db82685f2cc153c44adecb5
sunil <sunil@costacloud.com>
2024-06-13 11:04:36 +0530

    update

--- frontend/src/pages/AuthCallbackPage.tsx
+++ frontend/src/pages/AuthCallbackPage.tsx
@@ -58,7 +58,14 @@ const AuthCallbackPage = () => {
     user,
   ]);
 
-  return <>{createNewUserIsLoading ? <h1>Loading..</h1> : null}</>;
+  return <>{createNewUserIsLoading ?  <div className="flex flex-col items-center justify-center">
+    <h1 className="text-2xl font-bold mb-4 text-orange-500">
+      Loading...
+    </h1>
+    <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+      <div className="h-full bg-orange-500 rounded-full animate-pulse"></div>
+    </div>
+  </div> : null}</>;
 };
 
 export default AuthCallbackPage;
--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -32,7 +32,14 @@ const UserProfilePage = () => {
   return (
     <>
       {getCurrentUserIsLoading ? (
-        <div>Loading...</div>
+        <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold mb-4 text-orange-500">
+          Loading...
+        </h1>
+        <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+          <div className="h-full bg-orange-500 rounded-full animate-pulse"></div>
+        </div>
+      </div>
       ) : getCurrentUserIsError ? (
         <div>{getCurrentUserError}</div>
       ) : getCurrentUserIsSuccess ? (
