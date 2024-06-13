3f388c68d95960193f831b0aef4a85d938afa38d
sunil <sunil@costacloud.com>
2024-06-13 15:10:09 +0530

    update

---
+++ backend/src/routes/RestaurantRoute.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
+
+const router = express.Router();
+// /api/restaurant
+
+router.get(
+  "getRestaurant/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId paramenter must be a valid string"),
+  RestaurantController.getRestaurant
+);
+
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City paramenter must be a valid string"),
+  RestaurantController.searchRestaurant
+);
+
+export default router;
