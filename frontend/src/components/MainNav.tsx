7248c5b74655b85e9d4578552e2160b2f65d79a9
sunil <sunil@costacloud.com>
2024-06-13 12:27:02 +0530

    update

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -4,25 +4,41 @@ import UsernameMenu from "./UsernameMenu";
 import { Link } from "react-router-dom";
 
 const MainNav = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
 
   return (
     <span className="flex space-x-2 items-center">
-      {isAuthenticated ? (
-        <>
-          <Link to="/order-status" className="font-bold hover:text-orange-500">
-            Order Status
-          </Link>
-          <UsernameMenu />
-        </>
+      {isLoading ? (
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold mb-4 text-orange-500">
+            Loading...
+          </h1>
+          <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+            <div className="h-full bg-orange-500 rounded-full animate-pulse"></div>
+          </div>
+        </div>
       ) : (
-        <Button
-          variant="ghost"
-          className="font-bold hover:text-orange-500 hover:bg-white"
-          onClick={async () => await loginWithRedirect()}
-        >
-          Log In
-        </Button>
+        <>
+          {isAuthenticated ? (
+            <>
+              <Link
+                to="/order-status"
+                className="font-bold hover:text-orange-500"
+              >
+                Order Status
+              </Link>
+              <UsernameMenu />
+            </>
+          ) : (
+            <Button
+              variant="ghost"
+              className="font-bold hover:text-orange-500 hover:bg-white"
+              onClick={async () => await loginWithRedirect()}
+            >
+              Log In
+            </Button>
+          )}
+        </>
       )}
     </span>
   );
--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -13,7 +13,7 @@ import MobileNavLinks from "./MobileNavLinks";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const MobileNav = () => {
-  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, user, isLoading } = useAuth0();
 
   return (
     <Sheet>
@@ -22,22 +22,35 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className="space-y-3">
         <SheetTitle>
-          {isAuthenticated ? (
-            <span className="flex items-center font-bold gap-2">
-              <Avatar>
-                <AvatarImage src={user?.picture} />
-                <AvatarFallback>
-                  {user?.name
-                    ?.split(" ")
-                    .map((part) => part.charAt(0))
-                    .join("")}
-                </AvatarFallback>
-              </Avatar>
-
-              {user?.email}
-            </span>
+          {isLoading ? (
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-2xl font-bold mb-4 text-orange-500">
+                Loading...
+              </h1>
+              <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
+                <div className="h-full bg-orange-500 rounded-full animate-pulse"></div>
+              </div>
+            </div>
           ) : (
-            <span> Welcome to Food Delivery</span>
+            <>
+              {isAuthenticated ? (
+                <span className="flex items-center font-bold gap-2">
+                  <Avatar>
+                    <AvatarImage src={user?.picture} />
+                    <AvatarFallback>
+                      {user?.name
+                        ?.split(" ")
+                        .map((part) => part.charAt(0))
+                        .join("")}
+                    </AvatarFallback>
+                  </Avatar>
+
+                  {user?.email}
+                </span>
+              ) : (
+                <span> Welcome to Food Delivery</span>
+              )}
+            </>
           )}
         </SheetTitle>
         <Separator />
